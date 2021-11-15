7a125e670dd65c33555fe67c44ae9a40c5449d39
Roman Limonov <rlimonov.exceedteam@gmail.com>
2021-11-15 10:04:02 +0300

    fix bugs in controllers. remove frontend

--- Backend/src/modules/controllers/task.controller.js
+++ Backend/src/modules/controllers/task.controller.js
@@ -1,9 +1,18 @@
 const Task = require('../../db/models/task/index');
 
 module.exports.getAllTasks = (req, res, next) => {
-  Task.find().then(result => {
-    res.send({data: result});
-  })
+  try {
+    Task.find().then(result => {
+      res.send({data: result});
+    });
+  } catch (error) {
+    res.status(error.status || 500).send({
+      error: {
+        status: error.status || 500,
+        message: error.message || "Internal Server Error",
+      },
+    });
+  }
 }
 
 module.exports.createNewTask = (req, res, next) => {
@@ -14,32 +23,27 @@ module.exports.createNewTask = (req, res, next) => {
     task.save().then(result => {
       res.send(result);
     });
-
   } else {
-    res.send('Data is incorrect, error!');
+    res.status(422).send('Data is incorrect, error!');
   }
 }
 
 module.exports.changeTaskInfo = (req, res, next) => {
   const body = req.body;
 
-  if (body.hasOwnProperty('id') && body.hasOwnProperty('isCheck') && body.hasOwnProperty('text')) {
+  if (body.hasOwnProperty('id')) {
     Task.findOneAndUpdate({id: req.body.id}, {text, isCheck} = req.body, 
-      {upsert: true}).then(result => {
-        res.send(result);
-      })
+    {upsert: true}).then(result => {
+      res.send(result);
+    });
   } else {
     res.status(422).send('Data is incorrect, error!');
   }
 }
 
 module.exports.deleteTask = (req, res, next) => {
-  if(!req.query.id) {
+  if (!req.query.id) {
     return res.status(422).send('Error! Params not correct');
-
-  } else if (!Task.findOne({id: req.query.id})) {
-    res.status(404).send('Task not found');
-    
   } else {
     Task.deleteOne({id: req.query.id}).then(result => {
       res.send('succesfully deleted');
